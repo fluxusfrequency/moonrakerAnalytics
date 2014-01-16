a40ed47c814169a31adf535a40856692c5deab27
Ben Lewis <bennlewis@gmail.com>
2014-01-15 22:17:45 -0700

    got bootstrap working, yay

--- src/main/scala/com/moonrakerAnalytics/app/MyMoonrakerAnalyticsServlet.scala
+++ src/main/scala/com/moonrakerAnalytics/app/MyMoonrakerAnalyticsServlet.scala
@@ -29,8 +29,13 @@ class MyMoonrakerAnalyticsServlet extends MoonrakerAnalyticsStack {
   }
 
   notFound {
-    contentType="text/html"
-    ssp("/error")
+    // remove content type in case it was set through an action
+    contentType = null
+    // Try to render a ScalateTemplate if no route matched
+    findTemplate(requestPath) map { path =>
+      contentType = "text/html"
+      layoutTemplate(path)
+    } orElse serveStaticResource() getOrElse resourceNotFound()
   }
 
 }
