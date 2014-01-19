ebfc3247c99a76bb5b542ff6b0b4832c37e28db2
Ben Lewis <bennlewis@gmail.com>
2014-01-19 09:46:17 -0700

    sketch action model

--- src/main/scala/com/moonrakerAnalytics/app/models/Action.scala
+++ src/main/scala/com/moonrakerAnalytics/app/models/Action.scala
@@ -1,5 +1,19 @@
 package com.moonrakerAnalytics.app
 
-class Action {
-
+object Action {
+  val tableName = 0
+  val attributes = Seq("id", "requested_at", "responded_in", "request_type",
+                       "parameters", "digest", "source_id", "url_id",
+                       "user_agent_id", "resolution_id", "ip_id", "referrer_id",
+                       "event_id")
+  def runQuery {}
+  def urls {}
+  def browsers {}
+  def oss {}
+  def resolutions {}
+  def averageUrlResponseTime {}
+  def responseTimeForPath {}
+  def eventOccurranceByHour {}
+  def eventCount {}
+  def eventsSubset {}
 }
